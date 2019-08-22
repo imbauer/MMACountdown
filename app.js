4eabe8301dc43180e600defca71692f41a49969d
imbauer <ivan@imbauer.com>
2019-08-21 21:46:09 -0400

    Imporved parsing of UFC Events

--- app.js
+++ app.js
@@ -28,9 +28,9 @@ var weightClasses = ['Flyweight','Bantamweight','Featherweight','Lightweight','W
 var weightClassesVs = ['vs.','weight'];
 
 var tvSlots = ['Early Preliminary Card','Preliminary Card'];
-var regex = /(?=Early Preliminary Card)|(?=(?<!Early )Preliminary Card)/g;
-var weightRegex = /(?=\^ For)|(?=Flyweight)|(?=Bantamweight)|(?=Featherweight)|(?=Lightweight)|(?=Welterweight)|(?=Middleweight)|(?=(?<!Light )Heavyweight)|(?=Heavyweight)/g;
-var weightRegexAndVs = /(\s*(?:vs\.|^)\s*)|(\s*(?:Flyweight|^)\s*)|(?=Bantamweight)|(?=Featherweight)|(?=Welterweight)/g;
+var regex = /(?=Early Preliminary Card)|(?=(?<!Early )Preliminary Card)|(?=\^)/g;
+var weightRegex = /(?=\^)|(?=Women)|(?=(?<!Women..\s)Fly)|(?=(?<!Women..\s)Bantam)|(?=Featherweight)|(?=(?<!\s)Lightweight)|(?=Welterweight)|(?=Middleweight)|(?=(?<!Light )Heavyweight)|(?=Light Heavyweight)/g;
+var weightRegexAndVs = /(?:vs\.)|weight/g;
 
 app.get('/', function(request, response) {
   response.send('Hello World!')
@@ -39,14 +39,12 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 
-
 //var query = 'ufc%20242';
 //var query = 'UFC%20241:%20Cormier%20vs.%20Miocic%202'
 //var url = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${query}&format=json`;
-
-var url="https://en.wikipedia.org/w/api.php?action=parse&format=json&page=UFC%20242&prop=text";
+var currentEvent = 'UFC%20242'
+var url="https://en.wikipedia.org/w/api.php?action=parse&format=json&page=" + currentEvent + "&prop=text";
 //var url="https://en.wikipedia.org/w/api.php?action=parse&format=json&page=UFC%20Fight%20Night:%20Andrade%20vs.%20Zhang&prop=text";
-
 
   request(url, function (err, response, body) {
    if(err){
@@ -57,45 +55,37 @@ var url="https://en.wikipedia.org/w/api.php?action=parse&format=json&page=UFC%20
 //name,    title,               timezone, year, month,  day, weekDay, hour, minute, AMPM, nextEvent
 //UFC 241, CORMIER VS MIOCIC 2, EST,      2019, August, 17, Saturday, 10,   00,     PM
 
-
-//  body = body.replace(/<[^>]*>/g, "");
   body = body.replace(/<[^>]*>/g,'').replace(/\\n/g,'');
-//  console.log(body);
-
-//  console.log(processUFC(body));
-    processUFC(body);
-
-//  var arr = body.split("See also[edit]");
-//  var mainSection = arr[0].split("Fight Card");
-//  for (var i = 0; i < mainSection.length; i++) {
-//      var data = mainSection[i] + "\n";
-//        console.log(data);
-//        console.log();
-//        console.log();
-//  }
-//  console.log(arr[0]);
-//  console.log();
-////  console.log('body: ', body);
-//  console.log(arr[1]);
+    processUFC(body, currentEvent);
 
    }
   });
 });
-function processUFC(body) {
+function processUFC(body, currentEvent) {
+    currentEvent = currentEvent.replace('%20', ' ');
+
+    console.log('BODY');
+    console.log(body);
+    console.log();
+
     var arr = body.split("See also[edit]");
     var mainSection = arr[0].split("Fight Card");
-      for (var i = 0; i < mainSection.length; i++) {
-          var data = mainSection[i] + "\n";
-          console.log(tvSlots.join('|'));
-//          var tokens = data.split(new RegExp(tvSlots.join('|'), 'g'));
-
+        for (var i = 0; i < mainSection.length; i++) {
+            var data = mainSection[i] + "\n";
+            console.log(tvSlots.join('|'));
         if (i == 1) {
-          var tokens = data.split(regex);
-//          var tokens = data.split(new RegExp(weightClasses.join('|'), 'g'));
+            console.log(data);
+            data = data.substring(0, data.indexOf('Announced bout'));
+            var tokens = data.split(regex);
             console.log(tokens);
             for (var i = 0; i < tokens.length; i++) {
-//                var tokenSection = tokens[i].split(new RegExp(weightClasses.join('|'), 'g'));
                 var tokenSection = tokens[i].split(weightRegex);
+                if (i === 0) {
+                    tokenSection[0] = currentEvent;
+                }
+                else if (i === 1 || i === 2) {
+                    tokenSection[0] = currentEvent + ' ' + tokenSection[0];
+                }
                 console.log();
                 console.log('============== Token[' + i + ']============== ');
                 console.log(tokenSection);
@@ -103,24 +93,63 @@ function processUFC(body) {
                 console.log();
                 console.log('---TS---');
                 for (var j = 0; j < tokenSection.length; j++) {
-//                    console.log(tokenSection[j]);
-//                    var ts = tokenSection[j].split(weightRegexAndVs);
-                    var ts = tokenSection[j].split(new RegExp(weightClassesVs.join('|'),'g'));;
+                    var ts = tokenSection[j].split(weightRegexAndVs);
+                    if (j != 0 && ts.length === 3) {
+                        ts[0] = ts[0] + 'weight'
+                    }
                     console.log(ts);
-//                    for (var k = 0; k < ts.length; k++) {
-//                        console.log(ts[k]);
-//                    }
                 }
             }
 
         }
-            console.log(data);
             console.log();
             console.log();
       }
 //    return pet;
 }
-function processPFL() {
-     var pet = 'dog';
-     console.log(pet);
+function processPFL(body, currentEvent) {
+    currentEvent = currentEvent.replace('%20', ' ');
+
+    console.log('BODY');
+    console.log(body);
+    console.log();
+
+    var arr = body.split("See also[edit]");
+    var mainSection = arr[0].split("Fight Card");
+        for (var i = 0; i < mainSection.length; i++) {
+            var data = mainSection[i] + "\n";
+            console.log(tvSlots.join('|'));
+        if (i == 1) {
+            console.log(data);
+            data = data.substring(0, data.indexOf('Announced bout'));
+            var tokens = data.split(regex);
+            console.log(tokens);
+            for (var i = 0; i < tokens.length; i++) {
+                var tokenSection = tokens[i].split(weightRegex);
+                if (i === 0) {
+                    tokenSection[0] = currentEvent;
+                }
+                else if (i === 1 || i === 2) {
+                    tokenSection[0] = currentEvent + ' ' + tokenSection[0];
+                }
+                console.log();
+                console.log('============== Token[' + i + ']============== ');
+                console.log(tokenSection);
+                console.log('===================================== ');
+                console.log();
+                console.log('---TS---');
+                for (var j = 0; j < tokenSection.length; j++) {
+                    var ts = tokenSection[j].split(weightRegexAndVs);
+                    if (j != 0 && ts.length === 3) {
+                        ts[0] = ts[0] + 'weight'
+                    }
+                    console.log(ts);
+                }
+            }
+
+        }
+            console.log();
+            console.log();
+      }
+//    return pet;
 }
