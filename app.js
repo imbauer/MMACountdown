890c2a5a64be0e0d9a055ccde56eac2d654974e6
imbauer <ivan@imbauer.com>
2019-08-25 16:57:45 -0400

    Completed UFC portion of API (mostly)

--- app.js
+++ app.js
@@ -40,8 +40,9 @@ app.get('/', function(request, response) {
 
 app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'));
-
-    var currentEvent = 'UFC%20244'
+    // If event has a '+' in the title, run currentEvent.replace('+', '%2B');
+    var currentEvent = 'UFC%20242'
+//    currentEvent = 'UFC%20on%20ESPN%2B%2021';
 //    currentEvent = 'UFC%20Fight%20Night:%20Cerrone%20vs.%20Gaethje';
     var url = "https://en.wikipedia.org/w/api.php?action=parse&format=json&page=" + currentEvent + "&prop=text";
 //var url="https://en.wikipedia.org/w/api.php?action=parse&format=json&page=UFC%20Fight%20Night:%20Andrade%20vs.%20Zhang&prop=text";
@@ -78,12 +79,14 @@ function processUFC(body, currentEvent) {
             console.log(data);
             var info = data.split(infoRegex);
             info = info.filter(function(e){return e});
+            if (info[0].includes('+')) {
+                info[0] = info[0].replace('+', '\\+');
+            }
             if (info[0].includes('The poster')) {
                 info[0] = info[0].split('The poster')[0];
             }
             info[info.length - 1] = info[info.length - 1].match(new RegExp(info[0] + "(.*)" + info[0]))[1];
             info[2] = info[2].replace( /\W|[0-9]/g , '');
-            console.log(info);
 
             var country_list = ["USA","US","Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","St. Lucia","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","Uruguay","Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
 
@@ -93,7 +96,7 @@ function processUFC(body, currentEvent) {
 
             var locationCombined = info[7].split(', ');
             if (locationCombined.length < 3) {
-                console.log(locationCombined[1]);
+//                console.log(locationCombined[1]);
                 if (country_list.includes(locationCombined[1]) === true) {
                     city = locationCombined[0];
                     country = locationCombined[1];
@@ -111,7 +114,7 @@ function processUFC(body, currentEvent) {
             }
 
 
-            user.name = info[0].split(': ')[0];
+            user.name = info[0].replace('\\', '').split(': ')[0];
             user.title = info[0].split(': ')[1];
             user.nextEvent = info[info.length - 1];
 
@@ -184,7 +187,7 @@ function processUFC(body, currentEvent) {
     }
 
     user.fightCard = fightsTotal;
-
+    console.log(user.fightCard);
     return user;
 }
 function processPFL() {
