532670fce24f2ee7a0ee506bafe2c23ebae9794d
Ivan <ivan@imbauer.com>
2019-08-31 21:16:39 -0400

    Fixed event issue

--- scripts/process_promotions.js
+++ scripts/process_promotions.js
@@ -22,12 +22,7 @@ module.exports =
         var fightsTotal = [];
 
         body = body.replace(/<[^>]*>/g,'').replace(/\\n/g,'').replace(/\s\s+/g, ' ');
-        console.log(body);
-        console.log('^^BODY^^');
         var info = body.split(eventParsing);
-        console.log('===========INFO========');
-        console.log(info);
-        console.log('==========END INFO=====');
         info = info.filter(function(e){return e});
         var fightCard = info[7].split(splitEvents);
         info.pop();
@@ -44,9 +39,6 @@ module.exports =
         info[0] = info[0].replace(/(\|.*)|(=\s)/g, '');
         info[1] = info[1].replace(/(.*=\[\[)/g, '');
         info[2] = info[2].replace(/(date\|)/g, '').split('|');
-        console.log('===========INFO========');
-        console.log(info[3]);
-        console.log('==========END INFO=====');
 //        info[3] = info[3].replace(/(venue=)|(\[)|(\])/g, '').replace(/(.*=)|(.*\|)/g, '');
         info[3] = info[3].replace(/(\[)|(\])|(.*=)|(\|)/g, '').trim();
         info[4] = info[4].replace(/(city\s=)|(city=)|(\[)|(\])|(\|attendance.*)/g, '').replace(/(,.*\(state\)\|)/g, ', ');
@@ -54,9 +46,6 @@ module.exports =
         info[6] = info[6].replace(/(.*=\[\[)|(\|.*)/g, '');
 
 
-        console.log('===========INFO========');
-        console.log(info);
-        console.log('==========END INFO=====');
         var cycles = fightCard.length;
         if (cycles === 0) {
             cycles = 1;
@@ -74,14 +63,11 @@ module.exports =
             event.promotion = info[1];
             event.nextEvent = info[6];
             var location = {};
-            console.log('===========INFO========');
-            console.log(info[3]);
-            console.log('==========END INFO=====');
             location.name = info[3];
             var country_list = ["United States", "United States of America", "USA","US","Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","St. Lucia","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","Uruguay","Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
+            var unitedStatesList = ['Alabama','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Federated States of Micronesia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Marshall Islands','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Northern Mariana Islands','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming'];
+
             var locationCombined = info[4].split(', ');
-            console.log('------------------------');
-            console.log(locationCombined);
             for (var i = 0; i < locationCombined.length; i++) {
                 locationCombined[i] = locationCombined[i].trim();
             }
@@ -94,7 +80,12 @@ module.exports =
                 else {
                     location.city = locationCombined[0];
                     location.provState = locationCombined[1];
-                    location.country = "United States"
+                    if (unitedStatesList.includes(locationCombined[1]) === true) {
+                        location.country = "United States";
+                    }
+                    else if (unitedStatesList.includes(locationCombined[1]) === false) {
+                        location.country = "Brazil";
+                    }
                 }
             }
             else if (locationCombined.length === 3) {
