8ae443f3c6367e422747f53063d196bcb218cf94
imbauer <ivanbauer16@gmail.com>
2019-11-14 09:55:38 -0500

    Added www in front of url

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(mmacountdown.com mmacountdown.com)
+domains=(www.mmacountdown.com mmacountdown.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="vanyambauer@gmail.com" # Adding a valid address is strongly recommended
